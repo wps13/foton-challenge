6c414df3432ad8ae0a836075dc119ae548d04c97
wps13 <willanepaiva@hotmail.com>
2018-10-04 16:37:14 -0300

    Separate style from Login

--- frontend/src/modules/Auth/components/Login/Index.js
+++ frontend/src/modules/Auth/components/Login/Index.js
@@ -1,43 +1,21 @@
 import React from 'react'
-import { Button, TextInput, View, StyleSheet, Text } from 'react-native'
-
+import { Button, TextInput, View, Text } from 'react-native'
+import styles from './style'
 
 const Login = ({navigation}) => (
-    <View style={styles.view}>
+    <View className={styles.view}>
 
-        <View style={styles.viewInput}>
-            <TextInput textContentType="username" placeholder="User" autoCapitalize="none" style={styles.inputs} />
-            <TextInput textContentType="password" placeholder="*******" secureTextEntry={true} autoCapitalize="none" style={styles.inputs} />
+        <View className={styles.viewInput}>
+            <TextInput textContentType="username" placeholder="User" autoCapitalize="none" className={styles.inputs} />
+            <TextInput textContentType="password" placeholder="*******" secureTextEntry={true} autoCapitalize="none" className={styles.inputs} />
             <Button title="Sign In" color="black" />
 
         </View>
-        <View style={styles.viewReg}>
+        <View className={styles.viewReg}>
             <Text>New here?</Text>
             <Button title="Sign Up" color="black" onPress={() => navigation.navigate('Register')} />
         </View>
     </View>
 )
-const styles = StyleSheet.create(
-    {
-        inputs: {
-            height: "15%",
-            backgroundColor: "#ffffff",
-            margin:"5%",
-            justifyContent: "space-between"
-        },
-        view: {
-            flexDirection: "column",
-            justifyContent:"center",
-            flex: 1,
-        },
-        viewInput: {
-            flexDirection: "column",
-        },
-        viewReg: {
-            flexDirection: "column",
-            justifyContent: "space-between"
-        }
 
-    }
-)
 export default Login
---
+++ frontend/src/modules/Auth/components/Login/style.js
@@ -0,0 +1,27 @@
+import {StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create(
+    {
+        inputs: {
+            height: "15%",
+            backgroundColor: "#ffffff",
+            margin:"5%",
+            justifyContent: "space-between"
+        },
+        view: {
+            flexDirection: "column",
+            justifyContent:"center",
+            flex: 1,
+        },
+        viewInput: {
+            flexDirection: "column",
+        },
+        viewReg: {
+            flexDirection: "column",
+            justifyContent: "space-between"
+        }
+
+    }
+)
+
+export default styles
