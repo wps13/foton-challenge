379b168951350325f221ddad1b215e2fd2af6caa
wps13 <willanepaiva@hotmail.com>
2018-10-04 20:23:12 -0300

    Modify style in Login

--- frontend/src/modules/Auth/components/Login/Index.js
+++ frontend/src/modules/Auth/components/Login/Index.js
@@ -2,14 +2,13 @@ import React from 'react'
 import { Button, TextInput, View, Text } from 'react-native'
 import styles from './style'
 
-const Login = ({navigation}) => (
+const Login = ({ navigation }) => (
     <View style={styles.view}>
-
-        <View style={styles.viewInput}>
-            <TextInput textContentType="username" placeholder="User" autoCapitalize="none" style={styles.inputs} maxLength={20} />
-            <TextInput textContentType="password" placeholder="*******" secureTextEntry={true} autoCapitalize="none" style={styles.inputs} maxLength={20} />
+        <Text style={styles.text}>Welcome back!</Text>
+        <TextInput textContentType="username" placeholder="User" autoCapitalize="none" style={styles.inputs} maxLength={20} />
+        <TextInput textContentType="password" placeholder="*******" secureTextEntry={true} autoCapitalize="none" style={styles.inputs} maxLength={20} />
+        <View style={styles.buttonLogin} >
             <Button title="Sign In" color="black" />
-
         </View>
         <View style={styles.viewReg}>
             <Text style={styles.text}>New here?</Text>
--- frontend/src/modules/Auth/components/Login/style.js
+++ frontend/src/modules/Auth/components/Login/style.js
@@ -3,26 +3,39 @@ import {StyleSheet } from 'react-native'
 const styles = StyleSheet.create(
     {
         inputs: {
-            height: "15%",
+            height: "10%",
+            width: "60%",
             backgroundColor: "#ffffff",
-            margin:"5%",
-            justifyContent: "space-between"
+            margin: "2%",
+            justifyContent: "space-between",
+            borderRadius: 10,
+            padding: "2%",
+            alignSelf:"center",
+            flexDirection: "column",
         },
         view: {
             flexDirection: "column",
             justifyContent:"center",
             flex: 1,
         },
-        viewInput: {
-            flexDirection: "column",
+        buttonLogin: {
+            width: "60%",
+            margin: "2%",
+            borderRadius: 20,
+            alignSelf:"center",
         },
         viewReg: {
             flexDirection: "column",
-            justifyContent: "space-between"
+            width: "60%",
+            alignSelf: "center",
+            margin:"10%",
+            padding:"5%"
         },
         text:{
-            fontSize: 14,
+            fontSize: 20,
             fontWeight: "bold",
+            color: "black",
+            alignSelf:"center",
         }
     }
 )
