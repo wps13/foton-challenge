372d357f96dc8d7119d9c1a9addda07e63c293d2
wps13 <willanepaiva@hotmail.com>
2018-10-03 23:37:24 -0300

    Change flex

--- frontend/src/modules/Auth/components/Login/Index.js
+++ frontend/src/modules/Auth/components/Login/Index.js
@@ -4,13 +4,18 @@ import { Button, TextInput, View, StyleSheet } from 'react-native'
 
 const Login = () => (
     <View style={styles.view}>
+
         <View style={styles.viewInput}>
-            <TextInput textContentType="username" placeholder="User" autoCapitalize = "none" style={styles.inputs} />
-            <TextInput textContentType="password" placeholder="*******" secureTextEntry={true}  autoCapitalize = "none" style={styles.inputs} />
-            <Button title="Sign In"  color= "black" />
+            <TextInput textContentType="username" placeholder="User" autoCapitalize="none" style={styles.inputs} />
+            <TextInput textContentType="password" placeholder="*******" secureTextEntry={true} autoCapitalize="none" style={styles.inputs} />
+            <Button title="Sign In" color="black" />
+            <View style={styles.viewButton}>
+                <Button title="Sign Up" color="black" />
+            </View>
         </View>
-       
-       
+
+
+
         {/* <Button onPress={navigation.navigate('Register')} /> */}
     </View>
 )
@@ -20,23 +25,30 @@ const Login = () => (
 // }
 const styles = StyleSheet.create(
     {
-        inputs:{
-            height:"18%",
-            width:"100%",
-            margin:10,
+        inputs: {
+            height: "18%",
+            width: "100%",
             backgroundColor: "#ffffff",
-            flexDirection:"row",
-            flex:0.6,
+            flex: 0.1,
         },
-        view :{
-            alignSelf: "center",
-        },
-        viewInput:{
+        view: {
             flexDirection: "column",
-            flex:0.5,
-            alignSelf: "center",
+            flex: 1,
+            alignSelf : "center",
+            justifyContent:"space-between"
+        },
+        viewInput: {
+            flexDirection: "column",
+            flex: 0.4,
+            alignItems: "center",
+            justifyContent:"space-around"
+        },
+        viewButton: {
+            flexDirection: "column",
+            flex: 0.5,
+           justifyContent: "flex-end",
         }
-           
+
     }
 )
 export default Login
