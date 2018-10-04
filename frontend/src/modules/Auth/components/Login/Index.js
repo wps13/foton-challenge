66d7dc384f82880a00e7722fd44a7b7718000f28
wps13 <willanepaiva@hotmail.com>
2018-10-03 21:17:19 -0300

    Start Login Style

--- frontend/src/modules/Auth/components/Login/Index.js
+++ frontend/src/modules/Auth/components/Login/Index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Button, TextInput, View } from 'react-native'
-//import styles from './style.css'
+import { Button, TextInput, View, StyleSheet } from 'react-native'
+
 
 const Login = () => (
-    <View>
-        <TextInput textContentType="username" placeholder="User" autoCapitalize = "none" />
-        <TextInput textContentType="password" placeholder="*******" secureTextEntry={true}  autoCapitalize = "none"/>
+    <View style={styles.view}>
+        <TextInput textContentType="username" placeholder="User" autoCapitalize = "none" style={styles.inputs} />
+        <TextInput textContentType="password" placeholder="*******" secureTextEntry={true}  autoCapitalize = "none" style={styles.inputs} />
         {/* <Button value="Sign In" /> */}
         {/* <Button onPress={navigation.navigate('Register')} /> */}
     </View>
@@ -14,5 +14,16 @@ const Login = () => (
 // Login.navigationOptions = {
 //     title: 'Login',
 // }
-
+const styles = StyleSheet.create(
+    {
+        inputs:{
+            height:"15%",
+            width:"80%",
+            margin:10,
+        },
+        view :{
+            alignSelf: "center",
+        }
+    }
+)
 export default Login
