ffba4d3a3f691ae9efafc0db917e1449855d1d43
wps13 <willanepaiva@hotmail.com>
2018-10-03 22:09:39 -0300

    Add "Sign In" button

--- frontend/src/modules/Auth/components/Login/Index.js
+++ frontend/src/modules/Auth/components/Login/Index.js
@@ -4,9 +4,13 @@ import { Button, TextInput, View, StyleSheet } from 'react-native'
 
 const Login = () => (
     <View style={styles.view}>
-        <TextInput textContentType="username" placeholder="User" autoCapitalize = "none" style={styles.inputs} />
-        <TextInput textContentType="password" placeholder="*******" secureTextEntry={true}  autoCapitalize = "none" style={styles.inputs} />
-        {/* <Button value="Sign In" /> */}
+        <View style={styles.viewInput}>
+            <TextInput textContentType="username" placeholder="User" autoCapitalize = "none" style={styles.inputs} />
+            <TextInput textContentType="password" placeholder="*******" secureTextEntry={true}  autoCapitalize = "none" style={styles.inputs} />
+            <Button title="Sign In"  color= "black" />
+        </View>
+       
+       
         {/* <Button onPress={navigation.navigate('Register')} /> */}
     </View>
 )
@@ -17,13 +21,22 @@ const Login = () => (
 const styles = StyleSheet.create(
     {
         inputs:{
-            height:"15%",
-            width:"80%",
+            height:"18%",
+            width:"100%",
             margin:10,
+            backgroundColor: "#ffffff",
+            flexDirection:"row",
+            flex:0.6,
         },
         view :{
             alignSelf: "center",
+        },
+        viewInput:{
+            flexDirection: "column",
+            flex:0.5,
+            alignSelf: "center",
         }
+           
     }
 )
 export default Login
