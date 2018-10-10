ce64b3b779532859fcfdd950c6a17cf4ff35975e
wps13 <willanepaiva@hotmail.com>
2018-10-10 20:10:25 -0300

    Transform Auth/Login in stateful component

--- frontend/src/modules/Auth/components/Login/Index.js
+++ frontend/src/modules/Auth/components/Login/Index.js
@@ -1,33 +1,36 @@
 import React from 'react'
+import { Component } from 'react'
 import { Button, View, Text, ImageBackground } from 'react-native'
 import styles from './style'
 import Input from '../Input/Index'
 
-const Login = ({ navigation }) => (
+class Login extends Component({ navigation }) {
+    render() {
+        return (
 
-    <ImageBackground source={require("../../assets/images/rocks.jpg")} imageStyle={
-        { resizeMode: "stretch" }} style={{width: '100%', height: '100%'}}>
-        <View style={styles.container}>
-            <Text style={styles.text}>Welcome back!</Text>
+            <ImageBackground source={require("../../assets/images/rocks.jpg")} imageStyle={
+                { resizeMode: "stretch" }} style={{ width: '100%', height: '100%' }}>
+                <View style={styles.container}>
+                    <Text style={styles.text}>Welcome back!</Text>
 
-            <Input label="User" type="username" size={20} secure={false} />
-            <Input label="Password" type="password" size={20} secure={true} />
+                    <Input label="User" type="username" size={20} secure={false} />
+                    <Input label="Password" type="password" size={20} secure={true} />
 
-            <View style={styles.buttonLogin} >
-                <Button title="Sign In" color="black" />
-            </View>
+                    <View style={styles.buttonLogin} >
+                        <Button title="Sign In" color="black" />
+                    </View>
 
-            <View style={styles.viewReg}>
-                <Text style={styles.text}>New here?</Text>
-                <View style={styles.button}>
-                    <Button title="Sign Up" color="black" onPress={() => navigation.navigate('Register')} />
-                </View>
+                    <View style={styles.viewReg}>
+                        <Text style={styles.text}>New here?</Text>
+                        <View style={styles.button}>
+                            <Button title="Sign Up" color="black" onPress={() => navigation.navigate('Register')} />
+                        </View>
 
-            </View>
-        </View >
-    </ImageBackground>
-
-
-)
+                    </View>
+                </View >
+            </ImageBackground>
+        )
+    }
+}
 
 export default Login
