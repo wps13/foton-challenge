ac809506b2440a57693e17ca46ea3825d11dadb9
wps13 <willanepaiva@hotmail.com>
2018-10-23 19:41:21 -0300

    Transform home in stateful component

--- frontend/src/modules/Home/Index.js
+++ frontend/src/modules/Home/Index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Component } from 'react-native'
 import Button from '../Button/Index'
 
-const Home = () => (
-    <View>
-        <Buttom text={} />
-    </View>
-)
+class Home extends Component {
+    render() {
+        <View>
+            <Buttom text={} />
+        </View>
+    }
+}
+
 
