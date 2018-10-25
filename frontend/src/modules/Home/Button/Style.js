64bad54696f3fabf3e4de143115d10a16363336a
wps13 <willanepaiva@hotmail.com>
2018-10-25 09:15:43 -0300

    Add objects name in Button/Style

--- frontend/src/modules/Home/Button/Style.js
+++ frontend/src/modules/Home/Button/Style.js
@@ -2,7 +2,15 @@ import { StyleSheet } from 'react-native'
 
 const Styles = StyleSheet.create(
     {
+       View:{
 
+       }, 
+       Container:{
+
+       },
+       Text:{
+           
+       }
     }
 )
 
