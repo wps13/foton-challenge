838df246b6f6c7142aed8e6c2ffb54672bd5f008
Willane Paiva <19312323+wps13@users.noreply.github.com>
2022-07-08 22:31:39 -0300

    refactor: convert to function components

--- frontend/src/modules/Home/Index.js
+++ frontend/src/modules/Home/Index.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import {Component} from 'react';
 import {View} from 'react-native';
 import Button from './Button/Index';
 import Style from './Style';
 
-class Home extends Component {
-  render() {
-    return (
-      <View style={Style.container}>
-        <View style={Style.View}>
-          <Button
-            text="Create Product"
-            press={() => this.props.navigation.navigate('createProduct')}
-          />
-        </View>
-        <View style={Style.View}>
-          <Button text="List Product" />
-        </View>
+const Home = props => {
+  const {navigation} = props;
+  return (
+    <View style={Style.container}>
+      <View style={Style.View}>
+        <Button
+          text="Create Product"
+          press={() => navigation.navigate('createProduct')}
+        />
       </View>
-    );
-  }
-}
+      <View style={Style.View}>
+        <Button text="List Product" />
+      </View>
+    </View>
+  );
+};
 
 export default Home;
